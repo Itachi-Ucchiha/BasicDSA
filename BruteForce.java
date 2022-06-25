118bf597070892958ab4a2b4e6a354fd4c27f41c
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-06-25 10:29:24 +0530

    Update BruteForce.java

--- BruteForce.java
+++ BruteForce.java
@@ -1,5 +1,10 @@
-public class arrayExamples {
+import java.util.Arrays;
 
+
+
+public class arrayExamples {
+    // Time complexity = O(n^2)
+    // approach 1 ---------------------------------- worst approach
     static void approach_1(int [] arr, int n){   // nave approach (Brute force approach)
         boolean isMax;
         for(int i =0; i<arr.length; i++){
@@ -16,23 +21,12 @@ public class arrayExamples {
             }
         }
     }
- 
-
-    public static void main(String[] args) {
-
-    int [] arr = {95,12,41,56,44,231,1};
-    int n = arr.length;
-    approach_1(arr, n);       
-            
-            
-    }   
-}
 
 
 
-
-// Another approach by thking the 1st number as max
- static void approach_2(int[] arr, int n){
+//--------------------------------------------2nd approach -----------------------------------------
+    // time complexity = O(n)
+    static void approach_2(int[] arr, int n){
         int max = arr[0];
         for(int i=1;i<n;i++){
             if(arr[i]>max){
@@ -43,9 +37,30 @@ public class arrayExamples {
     }
 
 
+    
+    // ----------------------------------------- 3rd approach-------------------------------------------------
+    // by sorting the array in ascending order and finding the last element
+    // time complexity = O(n)
+    static void approach_3(int[] arr, int n){
+        Arrays.sort(arr);       // importesd the built in library for sorting the array
+        int max = arr[n-1];
+        System.out.println("max num is : "+max);
+    }
 
 
 
+    public static void main(String[] args) {
+    int [] arr = {95,12,41,56,44,231,1};
+    int n = arr.length;
+    approach_1(arr, n);       
+    approach_2(arr, n);       
+    approach_3(arr, n);       
+               
+    }   
+}
 
-// Output 
+
+
 // The greatest number is : 231
+// Largest element is : 231
+// max num is : 231
