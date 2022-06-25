922391e700b46ae1ac2f0db3015ed0c46c5f5db3
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-06-25 09:42:41 +0530

    max element in an array
    used no nav (Brute force method ) to find the max element in an array

---
+++ BruteForce.java
@@ -0,0 +1,29 @@
+public class arrayExamples {
+
+    static void approach_1(int [] arr, int n){   // nave approach (Brute force approach)
+        boolean isMax;
+        for(int i =0; i<arr.length; i++){
+            isMax=true;             // this flag value will be initialize everytime to true hwen the first loop will run again
+            for(int j = 0; j<n;j++){        // this 2nd loop will compare all the values with the 1st loop's index value
+                if(arr[j]>arr[i]){
+                    isMax = false;
+                    break;
+                }
+            }
+            if(isMax){      //at the end of the 2nd for loop this if will check the flag value 
+                System.out.println("The greatest number is : "+arr[i]);
+                return;
+            }
+        }
+    }
+ 
+
+    public static void main(String[] args) {
+
+    int [] arr = {95,12,41,56,44,231,1};
+    int n = arr.length;
+    approach_1(arr, n);       
+            
+            
+    }   
+}
