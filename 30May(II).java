6d3a7091700f3a44b7c8ddb4c8d171049c4ff69c
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-06-26 11:19:03 +0530

    Update 30May(II).java

--- 30May(II).java
+++ 30May(II).java
@@ -32,3 +32,13 @@ public class reverseArray {
 
     }
 }
+
+
+
+
+// ----------------------- Output -----------------------------
+
+// The initial array was:
+// {1,2,3,4,5,6,7,8,9,10,}
+// The reversed array is :
+// {10,9,8,7,6,5,4,3,2,1,}
