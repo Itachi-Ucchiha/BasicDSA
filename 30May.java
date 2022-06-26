f243fedcf4f3607ca9200662e4880996b2f9391d
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-06-26 09:50:43 +0530

    Bar Chart ,

---
+++ 30May.java
@@ -0,0 +1,31 @@
+public class BarChart {
+
+
+    static int maxEle(int[] arr){
+        int max = arr[0];
+        for(int i = 0 ; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {3,1,0,7,5};
+        int max = maxEle(arr);
+        for(int i=max; i>=1; i-- ) {
+            for(int j = 0; j<arr.length; j++){
+                if(i<=arr[j]){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
+  
