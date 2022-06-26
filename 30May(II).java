0c9d9cc0d3ea7014da1a5a441cf6639aa44d69f1
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-06-26 11:21:23 +0530

    reverse an array

--- 30May(II).java
+++ 30May(II).java
@@ -3,15 +3,17 @@ public class reverseArray {
     // two pointer approach --------------
     // with swapping logic...
     // 1st pointer at array[0]. 2nd pointer at array[array.length]
+    // TC = O(n)
     static void approach1(int[] arr){
         int i = 0;         // pointer 1
         int j = arr.length-1;      // pointer 2
+        int temp;
         while (i<j){
-            int temp = arr[i];      // swapping the last element with first
+            temp = arr[i];      // swapping the last element with first
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
-            j--;
+            j--;  
         }
         System.out.println("The reversed array is : ");
         System.out.print("{");
@@ -32,6 +34,7 @@ public class reverseArray {
 
     }
 }
+
 
 
 
