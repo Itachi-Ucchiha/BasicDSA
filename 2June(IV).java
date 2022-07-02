f1591bf0b39a1593cad1296608b9edb0afcd9474
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-07-02 05:33:23 +0530

    Majority Element

---
+++ 2June(IV).java
@@ -0,0 +1,80 @@
+public class MajorityElement {
+    
+
+
+    // brute froece appraoach -- compare the current element with the other elements of the array 
+    // TC = O(n^2) .... time limit exceeding method...
+    static int approach1(int[] nums){
+        int n = nums.length;
+        int majorityCount = n/2;    // the element should have apperaed more than n/2 times in array
+        for(int num : nums){
+            int count = 0;
+            for(int elem : nums){
+                if(elem == num){
+                    count+=1;
+                }
+            }
+            if(count>majorityCount){
+                return num;
+            }
+        }
+        return 0;
+    }
+
+
+
+
+    // approach 2 - hashmap 
+    static void approach2(int[] nums){
+
+    }
+
+
+
+
+
+    // appraoch 3 - Boyer moore Algorithm (basically vote algorithm- used in string questions)
+    // if the same element come increment count ... if other element apper decrement the count  
+    // TC = O(n) and SC = O(1) 
+    static int appraoch3(int[] nums){
+        int n = nums.length;
+        int count =0;
+        int majorityElement = 0;
+        for(int i =0;i<n;i++){
+            if(count==0){       // counter reset you can say
+                majorityElement = nums[i];
+                count++;
+            }
+            else if(majorityElement==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        // now to we have to check what's the last element inside the majorityElement ...
+        // and verify if it's lenght is greater than n/2
+//         count = 0;
+//         for(int i = 0 ; i<n ; i++){
+//             if(majorityElement==nums[i]){
+//                 count++;
+//             }
+//         }                                                                            
+                                                                          // if will run without the commented code
+                                                                        // because we assume that the majority element always exists in the array.
+//         int ans = count>n/2?majorityElement:0;
+//         return ans;
+      return majorityElement;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println(approach1(arr));
+
+    }
+}
+
+
+// output :
+// 3 
