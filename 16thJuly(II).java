bfc8d4c48339de1e08322bedfe4660b55c53cb08
Gaurav <98058499+Itachi-Ucchiha@users.noreply.github.com>
2022-07-27 09:13:47 +0530

    power calculate

---
+++ 16thJuly(II).java
@@ -0,0 +1,13 @@
+public class PowerCalculate {
+    
+    static int power(int base, int p){
+        if(p==0){
+            return 1;
+        }
+        return base*power(base, p-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 4));        
+    }
+}
